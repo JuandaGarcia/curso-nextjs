c84fad0be18585c3178b613b8c534a5127673d91
Juan David <gottygarcia@gmail.com>
2020-10-21 00:30:39 -0500

    fin del curso

---
+++ out/_next/static/gORor_EW2FyDRUisIspe2/_buildManifest.js
@@ -0,0 +1,38 @@
+self.__BUILD_MANIFEST = (function (a, b) {
+  return {
+    __rewrites: [],
+    '/': [
+      a,
+      b,
+      'static\u002Fchunks\u002Fpages\u002Findex-9197938c7af71f24ab96.js',
+    ],
+    '/_error': [
+      'static\u002Fchunks\u002Fpages\u002F_error-f09cb67d8bd8e24ae38e.js',
+    ],
+    '/about': [
+      a,
+      'static\u002Fchunks\u002Fpages\u002Fabout-5d23a2282c0668f3b7c1.js',
+    ],
+    '/cart': [
+      a,
+      'static\u002Fchunks\u002Fpages\u002Fcart-9de7048725005074ecc1.js',
+    ],
+    '/product/[id]': [
+      a,
+      b,
+      'static\u002Fchunks\u002Fpages\u002Fproduct\u002F[id]-4a0640dd51f6a67fa2b7.js',
+    ],
+    sortedPages: [
+      '\u002F',
+      '\u002F_app',
+      '\u002F_error',
+      '\u002Fabout',
+      '\u002Fcart',
+      '\u002Fproduct\u002F[id]',
+    ],
+  }
+})(
+  'static\u002Fchunks\u002F43bd46a068bfb2583012eef9c477242c49af3e55.9b7d53d09174b390ddcf.js',
+  'static\u002Fchunks\u002F86bd6d32e95993440c8793a61a113550b621c73d.96c491a02d44403170bb.js'
+)
+self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ out/_next/static/lJ2BwEZxHGvVNAk2G7QDs/_buildManifest.js
@@ -0,0 +1,43 @@
+self.__BUILD_MANIFEST = (function (a, b) {
+  return {
+    __rewrites: [],
+    '/': [
+      a,
+      b,
+      'static\u002Fchunks\u002Fpages\u002Findex-bdfe1affa9b686f6786c.js',
+    ],
+    '/_error': [
+      'static\u002Fchunks\u002Fpages\u002F_error-f09cb67d8bd8e24ae38e.js',
+    ],
+    '/about': [
+      a,
+      'static\u002Fchunks\u002Fpages\u002Fabout-5d23a2282c0668f3b7c1.js',
+    ],
+    '/cart': [
+      a,
+      'static\u002Fchunks\u002Fpages\u002Fcart-9de7048725005074ecc1.js',
+    ],
+    '/product/[id]': [
+      a,
+      b,
+      'static\u002Fchunks\u002Fpages\u002Fproduct\u002F[id]-b48dbcde42111b302078.js',
+    ],
+    '/yes-or-no': [
+      a,
+      'static\u002Fchunks\u002Fpages\u002Fyes-or-no-20541b106c39f463bdd3.js',
+    ],
+    sortedPages: [
+      '\u002F',
+      '\u002F_app',
+      '\u002F_error',
+      '\u002Fabout',
+      '\u002Fcart',
+      '\u002Fproduct\u002F[id]',
+      '\u002Fyes-or-no',
+    ],
+  }
+})(
+  'static\u002Fchunks\u002F34ae5396449522de849eabfa7feb09652b9ee636.9b7d53d09174b390ddcf.js',
+  'static\u002Fchunks\u002F86bd6d32e95993440c8793a61a113550b621c73d.96c491a02d44403170bb.js'
+)
+self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
